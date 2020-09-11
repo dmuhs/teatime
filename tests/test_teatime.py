578b7f920dcfe3b7349179d080b04d2c7d9aeefb
Dominik Muhs <dmuhs@protonmail.ch>
2020-09-11 13:23:11 +0200

    Fix tests

--- tests/test_teatime.py
+++ tests/test_teatime.py
@@ -1,23 +1,6 @@
-#!/usr/bin/env python
-
 """Tests for `teatime` package."""
 
-import pytest
 
-import teatime
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_content():
+    # TDD is dead!
+    pass
