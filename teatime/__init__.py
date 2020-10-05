a0ffd9177eeebee60ebffbbcb61fedfd18fbc26f
Dominik Muhs <dmuhs@protonmail.ch>
2020-10-05 21:49:07 +0200

    Improve top-level import hierarchy

--- teatime/__init__.py
+++ teatime/__init__.py
@@ -1,3 +1,8 @@
 """Let's toast some nodes, eh?"""
 
+from teatime.scanner import Scanner
+from teatime.reporting import Issue, Report, Severity
+from teatime.plugins.context import Context, NodeType
+from teatime.plugins.base import Plugin
+
 __version__ = "0.2.5"
