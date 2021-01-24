ecd7e996c023718f7cf4067d496241b0f3e09131
Dominik Muhs <dmuhs@protonmail.ch>
2021-01-24 22:13:08 +0100

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     url="https://github.com/dmuhs/teatime",
-    version="0.3.0",
+    version="0.3.1",
     zip_safe=False,
 )
